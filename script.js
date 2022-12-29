06ec0fb6741f545fa90217be5857e64911afa5b7
ramona <ramonaejspence@gmail.com>
2022-12-29 15:50:52 -0500

    remove notes when it passes through target

--- script.js
+++ script.js
@@ -19,10 +19,28 @@ function initializeGame() {
 
 function initializeNotes() {
     // get width and height of game area
-
+    const gameWidth = document.getElementById("game").style.width;
+    const gameHeight = document.getElementById("game").style.height;
     // calculate size and position of notes
-
+    const noteSize = 125;
+    const noteSpacing = 10;
+    const totalNoteWidth = noteSize * 4 + noteSpacing * 3;
+    const startX = (gameWidth - totalNoteWidth) / 16;
+    const startY = 0;
     // create notes
+    for (let i = 0; i < 4; i++) {
+        const note = document.createElement('div');
+        note.style.width = noteSize + 'px';
+        note.style.height = noteSize + 'px';
+        note.style.backgroundColor = 'lavender';
+        note.style.position = 'absolute';
+        note.style.left = startX + i * (noteSize + noteSpacing) + 'px';
+        note.style.top = startY + 'px';
+        document.getElementById("notes").appendChild(note);
+        notes.push(note)
+      }
+      console.log(notes)
+      return notes
 }
 
 function updateGameState() {
@@ -31,7 +49,18 @@ function updateGameState() {
         let note = notes[i];
         let y = parseInt(note.style.top);
         note.style.top = (y + 1) + "px";
-    }
+    
+
+        // Check if the note has reached the bottom of the screen
+        let target = document.getElementById("target")
+        console.log(target.style.top)
+        if (y >= 800) {
+        // If the note has reached the bottom, remove it from the game
+        notes.splice(i, 1);
+        i--;
+        note.remove();
+        }
+    }   
 }
 
 window.addEventListener('load', initializeGame, false);
