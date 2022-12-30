35b77dfc31a53af223013b47ecad49c4d19eca0d
ramona <ramonaejspence@gmail.com>
2022-12-30 11:55:50 -0500

    use fixed note width + cleanup

--- script.js
+++ script.js
@@ -2,45 +2,38 @@
 const spell = {'a': 1000, 's':2000, 'd':3000, 'f':4000,}
 const notes = []
 
-class Timer {
-    constructor() {
-        this.start = Date.now();
-        this.time = Date.now() - this.start;
-    }
+function isHit() {
+    // adds to score
 }
 
-function initializeGame() {
-    // start timer
-    const timer = new Timer();
+function isMiss() {
+    // deducts from score
+}
+
+function judgeScore() {
+    // decides if spell succeeded or failed based on score
+}
+
+function initializeGame(spell) {
     // initalize notes with spell
-    initializeNotes();
+    initializeNotes(spell);
     // run game, updating state thousandth of a millisecond (??)
-    setInterval(updateGameState, 1000/1000) 
+    setInterval(updateGameState, 1)
 }
 
-function initializeNotes() {
-    // get width and height of game area
-    const gameWidth = document.getElementById("game").style.width;
-    const gameHeight = document.getElementById("game").style.height;
-    // calculate size and position of notes
+function addNote(key) {
+    const column = 125;
     const noteSize = 125;
     const noteSpacing = 10;
-    const totalNoteWidth = noteSize * 4 + noteSpacing * 3;
-    const startX = (gameWidth - totalNoteWidth) / 16;
     const startY = 0;
-    // create notes
-    for (let i = 0; i < 4; i++) {
-        const note = document.createElement('div');
-        note.style.width = noteSize + 'px';
-        note.style.height = noteSize + 'px';
-        note.style.backgroundColor = 'lavender';
-        note.style.position = 'absolute';
-        note.style.left = startX + i * (noteSize + noteSpacing) + 'px';
-        note.style.top = startY + 'px';
-        document.getElementById("notes").appendChild(note);
-        notes.push(note)
-    // }
-
+    const note = document.createElement('div');
+    note.style.width = column + 'px';
+    note.style.height = noteSize + 'px';
+    note.style.backgroundColor = 'lavender';
+    note.style.position = 'absolute';
+    note.style.top = startY + 'px';
+    document.getElementById(key).appendChild(note);
+    notes.push(note)
 }
 
 function initializeNotes(spell, start) {
@@ -56,10 +49,8 @@ function updateGameState() {
         let y = parseInt(note.style.top);
         note.style.top = (y + 1) + "px";
     
-
         // Check if the note has reached the bottom of the screen
         let target = document.getElementById("target")
-        console.log(target.style.top)
         if (y >= 800) {
         // If the note has reached the bottom, remove it from the game
         notes.splice(i, 1);
@@ -69,4 +60,4 @@ function updateGameState() {
     }   
 }
 
-window.addEventListener('load', initializeGame, false);
+document.getElementById("startButton").addEventListener('click', function(){initializeGame(spell)}, false);
